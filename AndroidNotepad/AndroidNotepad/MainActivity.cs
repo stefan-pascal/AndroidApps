5ecab3c2dd5d5b845af6aef47e2c75bf38ea2188
Stefan Pascal <stefan.pascal@gmail.com>
2017-05-29 10:17:20 +0300

    Version 0.1

---
+++ AndroidNotepad/AndroidNotepad/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace AndroidNotepad
+{
+	[Activity(Label = "AndroidNotepad", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			MobileCenter.Start("32267c0e-f758-44b2-a8c4-3e8b5f8b6d9c",
+				   typeof(Analytics), typeof(Crashes));
+
+			// Set our view from the "main" layout resource
+			// SetContentView (Resource.Layout.Main);
+		}
+	}
+}
+
